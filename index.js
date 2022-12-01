916e792c40ee84b65b92980318ea149b71cbb8ad
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-01 10:21:41 +0300

    completed assignement

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+// //- `titleCased()`: returns an array with title case tutorial names. Note that
+// this function takes no arguments and should use the global `tutorials`
+// variable as data.
+//had to remove the array mo
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((array) => {
+    const firstArray = array.split(' ')
+    //returns the map of the whole array. Removing need to have a FOR loop.
+  //the arrow function then invokes a variable which = each line of array and splits to individual words
+  //placing in secondArray.
+    
+    const secondArray = firstArray.map((string) => string.charAt(0).toUpperCase() + string.slice(1))
+     //a second variable is invoked which = the map of the new array of words,
+  //it then takes first letter of word[0] and puts toUpperCase and holds the rest of the word back to be joined back
+    return secondArray.join(' ') //then returns updated array.
+  })
 }
